f89fd2bd10978b1ce78d2e254e0d8024c6bc38c3
xlzior <wenjun.lye@gmail.com>
2021-11-30 11:41:22 +0800

    Add LifeXP to projects

--- src/components/TagsList/index.js
+++ src/components/TagsList/index.js
@@ -3,12 +3,16 @@ import { Tag } from 'antd';
 
 const LANGUAGES = '#faad14';
 const FRAMEWORKS = '#2db7f5';
+const API = '#2db7f5';
 const FIELDS = '#87d068';
 
 const getTagColour = name => {
   switch (name) {
     case 'JavaScript':
+    case 'TypeScript':
+    case 'Node.js':
     case 'Python':
+    case 'Postgresql':
       return LANGUAGES;
     case 'React':
     case 'Redux':
@@ -16,11 +20,15 @@ const getTagColour = name => {
     case 'scrapy':
     case 'Google App Engine':
       return FRAMEWORKS;
+    case 'Telegram Bot API':
+      return API;
     case 'web development':
     case 'app development':
     case 'web scraping':
     case 'data science':
       return FIELDS;
+    case 'Heroku':
+      return '#79589F'
     default:
       return null;
   }
