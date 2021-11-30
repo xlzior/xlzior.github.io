5d758a7454609fa00fcdbaf146a5d385f0cadf17
xlzior <wenjun.lye@gmail.com>
2021-11-30 12:43:00 +0800

    Fix HashBrowser interfering with Anchor Links

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -14,18 +14,18 @@ const Resume = () => (
     <p>Hello! I'm Wen Jun. I picked up programming on my own in 2014 and have been hooked ever since. I enjoy using technology to create things to help people in their daily lives.</p>
 
     <Anchor>
-      <Anchor.Link href="#work-experience" title="Work Experience" />
-      <Anchor.Link href="#projects" title="Projects" />
-      <Anchor.Link href="#competitions" title="Competitions" />
+      <Anchor.Link href="#/resume/#work-experience" title="Work Experience" />
+      <Anchor.Link href="#/resume/#projects" title="Projects" />
+      <Anchor.Link href="#/resume/#competitions" title="Competitions" />
     </Anchor>
     
-    <h2 id="work-experience">Work Experience</h2>
+    <h2 id="/resume/#work-experience">Work Experience</h2>
     <Timeline data={work} />
 
-    <h2 id="projects"><Link to="/projects">Projects</Link></h2>
+    <h2 id="/resume/#projects"><Link to="/projects">Projects</Link></h2>
     <Timeline data={projects} isProject />
 
-    <h2 id="competitions">Competitions</h2>
+    <h2 id="/resume/#competitions">Competitions</h2>
     <Timeline data={competitions} />
     <div className="spacer" />
   </Page>
